d78d4a5ce9cbe88ab9ebdaafd4777ea93353fd49
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-25 18:16:24 +0900

    feat: actions, mutations 쓰는 방법, store state 접근 방법

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { fetchNewsList } from "@/api";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     news: [],
+  },
+  mutations: {
+    //mutations type명(state, api data 인자값)
+    SET_NEWS(state, news) {
+      state.news = news;
+    },
+  },
+  actions: {
+    FETCH_NEWS(context) {
+      fetchNewsList()
+        .then((response) => {
+          console.log(response.data);
+          //mutations에 넣는 방법 - context.commit(mutations type명, 값)
+          context.commit("SET_NEWS", response.data);
+        })
+        .catch((e) => console.log(e));
+    },
   },
 });
--- src/view/NewsView.vue
+++ src/view/NewsView.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
-    <div v-for="item in news" v-bind:key="item.title">{{ item.title }}</div>
+    <div v-for="item in this.$store.state.news" v-bind:key="item.title">
+      {{ item.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from "../api/index";
 export default {
-  data() {
-    return {
-      news: [],
-    };
-  },
   created() {
-    fetchNewsList()
-      .then((response) => (this.news = response.data))
-      .catch((error) => console.log(console.log(error)));
+    //dispatch
+    this.$store.dispatch("FETCH_NEWS");
   },
 };
 </script>
