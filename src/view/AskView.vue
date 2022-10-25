8400996f0a44b727939a8a281a4b5e3d058d5dec
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-25 18:30:29 +0900

    feat: computed에 mapState를 이용해서 vuex bindings

--- src/view/AskView.vue
+++ src/view/AskView.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <div v-for="item in this.$store.state.ask" v-bind:key="item.title">
+    <div v-for="item in ask" v-bind:key="item.title">
       {{ item.title }}
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
+  //this. 타고타고 많아져서 mapState를 씀
+  computed: {
+    ...mapState({
+      ask: (state) => state.ask,
+    }),
+  },
   created() {
     this.$store.dispatch("FETCH_ASK");
   },
