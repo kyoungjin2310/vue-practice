d1fb1aa66f4f6d7e1a78b0228776246b3aa85dbd
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-25 16:55:34 +0900

    feat: v-for, v-bind:key, api 기본

--- src/view/NewsView.vue
+++ src/view/NewsView.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" v-bind:key="user">{{ user }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => (this.users = response.data))
+      .catch((error) => console.log(console.log(error)));
+  },
+};
 </script>
 
 <style></style>
