d860ccbd030f17f1e862575b12e26d6e6621d359
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-25 15:48:25 +0900

    feat: router 기본 setting

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+import AskView from "../view/AskView.vue";
+import NewsView from "../view/NewsView.vue";
+import JobsView from "../view/JobsView.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: "/news",
+      component: NewsView,
+    },
+    {
+      path: "ask",
+      component: AskView,
+    },
+    {
+      path: "jobs",
+      component: JobsView,
+    },
+  ],
+});
