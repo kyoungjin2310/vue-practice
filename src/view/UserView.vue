a63dfbb7d547309ba61670dc0b5b873f1d31c5ab
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-26 14:19:49 +0900

    feat: [프로필 - router] router params id 값을 활용

--- src/view/UserView.vue
+++ src/view/UserView.vue
@@ -3,7 +3,13 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  created() {
+    const userName = this.$route.params.id;
+    axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
+  },
+};
 </script>
 
 <style></style>
