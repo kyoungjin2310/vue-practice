30f050c4b2db61dde49f507a957e963d99b46168
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-26 15:00:14 +0900

    feat: 객체타고 타고 들어가는 것 축약 - computed 사용

--- src/view/UserView.vue
+++ src/view/UserView.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <p>{{ this.$store.state.user.karma }}</p>
+    <p>name: {{ userInfo.name }}</p>
+    <p>karma: {{ userInfo.karma }}</p>
+    <p>created: {{ userInfo.created }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    },
+  },
   created() {
     const userName = this.$route.params.id;
     //.dispatch("type", 인자) - 인자가 하나만 넘어가서 여러 값을 넘길 때에는 객체로 넘기기
