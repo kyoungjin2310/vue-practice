0652a3b9a4d576876977940bc8fd3d6024e61d96
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-26 13:47:56 +0900

    feat: v-bind url 나머지 작업

--- src/view/NewsView.vue
+++ src/view/NewsView.vue
@@ -2,6 +2,7 @@
   <div>
     <p v-for="item in this.$store.state.news" v-bind:key="item.title">
       <a v-bind:href="item.url">{{ item.title }}</a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
     </p>
   </div>
 </template>
