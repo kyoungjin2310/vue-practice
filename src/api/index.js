02d1e24f6aab019078182fcec06e18cd6f3e2a1f
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-28 15:01:14 +0900

    feat:[비동기 - async, await] async, await 처리 방식

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,13 @@ const config = {
   baseUrl: "https://api.hnpwa.com/v0/",
 };
 
-//하이오더 컴포넌트 api news ~ jobs
 function fetchList(pageName) {
-  return axios.get(`${config.baseUrl}${pageName}/1.json`);
+  try {
+    const response = axios.get(`${config.baseUrl}${pageName}/1.json`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //router params id값을 인자로 받아서 api요청
--- src/store/actions.js
+++ src/store/actions.js
@@ -2,30 +2,27 @@ import { fetchUserInfo, fetchCommentItem, fetchList } from "@/api";
 
 export default {
   // network slow 3g 하면 spinner 오류
-  FETCH_USER({ commit }, name) {
-    return fetchUserInfo(name)
-      .then((response) => {
-        commit("SET_USER", response.data);
-        return response;
-      })
-      .catch((e) => console.log(e));
+  async FETCH_USER({ commit }, name) {
+    try {
+      const response = fetchUserInfo(name);
+      commit("SET_USER", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_ITEM({ commit }, id) {
-    return fetchCommentItem(id)
-      .then((response) => {
-        commit("SET_ITEM", response.data);
-        return response;
-      })
-      .catch((e) => console.log(e));
+  async FETCH_ITEM({ commit }, id) {
+    try {
+      const response = await fetchCommentItem(id);
+      commit("SET_ITEM", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_LIST({ commit }, pageName) {
-    //return을 꼭 써야 비동기 처리가 됨
-    return fetchList(pageName)
-      .then((response) => {
-        commit("SET_LIST", response.data);
-        //여기도 씀
-        return response;
-      })
-      .catch((e) => console.log(e));
+  async FETCH_LIST({ commit }, pageName) {
+    const response = await fetchList(pageName);
+    commit("SET_LIST", response.data);
+    return response;
   },
 };
