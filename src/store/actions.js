c2a87dda80d7a31d944b5554dfa44c1b5e59c90e
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-28 11:09:50 +0900

    fix:[spinner -  비동기 처리] network slow 3g 오류 수정

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,9 @@
 import { fetchUserInfo, fetchCommentItem, fetchList } from "@/api";
 
 export default {
+  // network slow 3g 하면 spinner 오류
   FETCH_USER({ commit }, name) {
-    fetchUserInfo(name)
+    return fetchUserInfo(name)
       .then((response) => {
         commit("SET_USER", response.data);
         return response;
@@ -10,7 +11,7 @@ export default {
       .catch((e) => console.log(e));
   },
   FETCH_ITEM({ commit }, id) {
-    fetchCommentItem(id)
+    return fetchCommentItem(id)
       .then((response) => {
         commit("SET_ITEM", response.data);
         return response;
@@ -18,8 +19,13 @@ export default {
       .catch((e) => console.log(e));
   },
   FETCH_LIST({ commit }, pageName) {
-    fetchList(pageName)
-      .then(({ data }) => commit("SET_LIST", data))
+    //return을 꼭 써야 비동기 처리가 됨
+    return fetchList(pageName)
+      .then((response) => {
+        commit("SET_LIST", response.data);
+        //여기도 씀
+        return response;
+      })
       .catch((e) => console.log(e));
   },
 };
