9545786379609d03a3c839fca77fa0f18c3f4b05
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-29 17:21:58 +0900

    feat: renderless component

---
+++ src/components/FetchData.vue
@@ -0,0 +1,36 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["url"],
+  data() {
+    return {
+      response: null,
+      loading: true,
+    };
+  },
+  created() {
+    axios
+      .get(this.url)
+      .then((res) => {
+        this.response = res.data;
+        this.loading = false;
+      })
+      .catch((e) => {
+        console.log(e);
+        alert("[ERROR] fetching the data", e);
+      });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading,
+    });
+  },
+};
+</script>
+
+<style></style>
--- src/view/InputTest.vue
+++ src/view/InputTest.vue
@@ -8,13 +8,15 @@
       v-on:input="checkedBox"
     />
     <!-- v-on:input="checkedBox" - v-on:하위 컴포넌트에서 발생한 이벤트 이름="상위 컴포넌트의 메서드 이름"-->
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1" />
   </div>
 </template>
 
 <script>
 import CheckBox from "../components/CheckBox.vue";
+import FetchData from "../components/FetchData.vue";
 export default {
-  components: { CheckBox },
+  components: { CheckBox, FetchData },
   data() {
     return {
       list: ["w1"],
